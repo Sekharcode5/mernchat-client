4a05f6ef1bfe2cc31332179439fcbb7f55080361
Sekharcode5 <vadlapudisekhar987@gmail.com>
2024-08-05 23:40:51 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:3000';
+  axios.defaults.baseURL = 'https://merchat-api.vercel.app/';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
